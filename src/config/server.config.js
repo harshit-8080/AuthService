964d3c21a97399e3f46afd0d93546af5736513d5
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-12-24 23:01:01 +0530

    signup password hashing feature added

--- src/config/server.config.js
+++ src/config/server.config.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
+const bcrypt = require('bcrypt');
 
 const PORT = process.env.PORT;
+const SALT = bcrypt.genSaltSync(Number(process.env.SALTROUNDS));
+
 
 module.exports = {
   PORT,
+  SALT
 };
